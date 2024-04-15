1cc1f9d53b9150ac0ee9f90786426aa1de1a675b
Wendy <wendyanto.bukitvista@gmail.com>
2024-04-15 13:40:30 +0700

    custom button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+interface IButtonProps {
+    text: string;
+    rounded?: "small" | "medium" | "large";
+    height: string;
+    width: string;
+    classes?: string;
+}
+
+function Button({ text, rounded = "small", height, width, classes = "" }: IButtonProps) {
+    const roundedValues = {
+        small: "10px",
+        medium: "15px",
+        large: "20px"
+    };
+
+    return (
+        <StyledButton $height={height} $width={width} $rounded={roundedValues[rounded]} className={classes}>
+            {text}
+        </StyledButton>
+    );
+}
+
+const StyledButton = styled.button.attrs<{ $height: string; $width: string; $rounded?: string }>(
+    ({ $height, $width, $rounded }) => ({
+        $height,
+        $width,
+        $rounded
+    })
+)`
+    height: ${(props) => props.$height};
+    width: ${(props) => props.$width};
+    border-radius: ${(props) => props.$rounded};
+`;
+
+export default Button;
