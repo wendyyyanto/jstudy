93b2c2656315a36cc6136eca25e2e077906568fb
Wendy <wendyanto.bukitvista@gmail.com>
2024-04-15 11:01:56 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import illustration from "assets/illustration.svg";
+
+function App() {
+    return (
+        <main>
+            <img src={illustration} alt="Landing Page Illustration" />
+        </main>
+    );
+}
+
+export default App;
