141b8498ccf925f5bea9505ed79dc16fec3b9d2c
Wendy <wendyanto.bukitvista@gmail.com>
2024-04-24 11:39:49 +0700

    improve styling

--- src/pages/Dashboard/components/Info/LastModuleInfo.tsx
+++ src/pages/Dashboard/components/Info/LastModuleInfo.tsx
@@ -10,21 +10,21 @@ interface ILastModuleInfoProps {
 function LastModuleInfo({ courseName, lastAccessed, address }: ILastModuleInfoProps) {
     return (
         <>
-            <div className="flex gap-2 text-base">
-                <LuTerminal size={20} />
-                <p className="text-caption-regular text-stroke-300">Last module accessed by you</p>
+            <div className="flex gap-2 items-center">
+                <LuTerminal size={22} />
+                <p className="text-p1-regular text-stroke-300">Last module accessed by you</p>
             </div>
 
-            <p className="text-subheading-semibold text-stroke-800 mt-5 w-10/12 text-ellipsis whitespace-nowrap overflow-hidden">
+            <p className="text-h5-semibold text-stroke-800 mt-5 w-10/12 text-ellipsis whitespace-nowrap overflow-hidden">
                 {courseName}
             </p>
-            <p className="text-caption-regular text-stroke-300">Last accessed {lastAccessed}</p>
+            <p className="text-p1-regular text-stroke-300 mt-2">Last accessed {lastAccessed}</p>
 
             <a
                 href={address}
-                className="flex justify-end gap-2 items-center underline text-caption-regular text-stroke-800 mt-8"
+                className="flex justify-end gap-2 items-center underline text-p2-regular text-stroke-800 mt-8"
             >
-                Continue <HiArrowRight size={14} />
+                Continue <HiArrowRight size={18} />
             </a>
         </>
     );
