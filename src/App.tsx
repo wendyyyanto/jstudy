fac601dbd0701fdde2f4a2041c3d026c2b678ef8
Wendy <wendyanto.bukitvista@gmail.com>
2024-04-24 11:40:01 +0700

    refactor routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,6 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import Dashboard from "@/pages";
-
-import Home from "pages/Home";
-import DashboardPage from "pages/Dashboard";
-import AchievementsPage from "pages/Achievements";
-import CoursesPage from "pages/Courses";
-import ChallengePage from "pages/Challenge";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />
-    },
-    {
-        path: "dashboard",
-        element: <Dashboard />,
-        children: [
-            {
-                path: "",
-                element: <DashboardPage />
-            },
-            {
-                path: "courses",
-                element: <CoursesPage />
-            },
-            {
-                path: "challenge",
-                element: <ChallengePage />
-            },
-            {
-                path: "achievements",
-                element: <AchievementsPage />
-            }
-        ]
-    }
-]);
+import router from "./routes/Router";
 
 function App() {
     return (
---
+++ src/routes/Router.tsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Dashboard from "@/pages";
+
+import Home from "pages/Home";
+import DashboardPage from "pages/Dashboard";
+import AchievementsPage from "pages/Achievements";
+import CoursesPage from "pages/Courses";
+import ChallengePage from "pages/Challenge";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "dashboard",
+        element: <Dashboard />,
+        children: [
+            {
+                path: "",
+                element: <DashboardPage />
+            },
+            {
+                path: "courses",
+                element: <CoursesPage />
+            },
+            {
+                path: "challenge",
+                element: <ChallengePage />
+            },
+            {
+                path: "achievements",
+                element: <AchievementsPage />
+            }
+        ]
+    }
+]);
+
+export default router
