97577f475ac3dbf74da72095c94ee03a354691bc
Wendy <wendyanto.bukitvista@gmail.com>
2024-04-24 12:07:19 +0700

    replace button with NavLink

--- src/pages/Home/components/NavBar.tsx
+++ src/pages/Home/components/NavBar.tsx
@@ -1,22 +1,19 @@
-import Button from "components/Button";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
     return (
         <section className="flex h-[120px] items-center justify-between px-[60px]">
             <h5 className="text-h5-bold">JStudy.</h5>
             <div className="flex items-center gap-8">
-                <Button
-                    height="auto"
-                    width="auto"
-                    classes="text-h6-regular text-stroke-500 h-auto w-auto"
-                    text="Log-in"
-                />
-                <Button
-                    height="3.5rem"
-                    width="7.6rem"
-                    text="Sign up"
-                    classes="text-h6-regular text-stroke-500 bg-highlight-400"
-                />
+                <NavLink to={"/auth/signin"} className="text-h6-regular text-stroke-500 h-auto w-auto">
+                    Log-in
+                </NavLink>
+                <NavLink
+                    to={"/auth/signup"}
+                    className="flex text-h6-regular text-stroke-500 bg-highlight-400 rounded-sm justify-center items-center h-[3.5rem] w-[7.6rem]"
+                >
+                    Sign-Up
+                </NavLink>
             </div>
         </section>
     );
