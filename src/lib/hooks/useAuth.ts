19f36dd27c0beb61d4c0ee783e0c98ea383cf8fa
Wendy <wendyanto.bukitvista@gmail.com>
2024-05-01 11:48:52 +0700

    update has_finished and has_failed to false if day changed and reset streak to 0 if user not attemping challenge in 24hour

--- src/lib/hooks/useAuth.ts
+++ src/lib/hooks/useAuth.ts
@@ -1,15 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 import supabase from "@/lib/supabaseClient";
 import useStudentContext from "@/context/studentContext";
 import { useStudentApi } from "@/api/student";
 import { User } from "@supabase/supabase-js";
+import { Tables, TablesUpdate } from "@/types/database.types";
+
+dayjs.extend(relativeTime);
 
 const useAuth = () => {
     const navigate = useNavigate();
 
     const { setStudent } = useStudentContext();
-    const { getStudent } = useStudentApi();
+    const { getStudent, updateStudent } = useStudentApi();
 
     const getUser = async () => {
         const {
@@ -27,6 +32,8 @@ const useAuth = () => {
         }
 
         setStudent(student);
+
+        return student;
     };
 
     const handleAuthenticatedUser = async () => {
@@ -37,6 +44,39 @@ const useAuth = () => {
         navigate("/dashboard");
     };
 
+    const handleResetOnLoad = async (student: Tables<"students">) => {
+        await handleResetStreak(student);
+    };
+
+    const handleResetChallengeAttempt = async (student: Tables<"students">) => {
+        const { last_challenge_timestamp } = student;
+
+        const isNewDay = dayjs().isAfter(last_challenge_timestamp, "d");
+
+        if (isNewDay) {
+            const newStudent: TablesUpdate<"students"> = {
+                has_failed_challenge: false,
+                has_finished_challenge: false
+            };
+
+            const updatedStudent = await updateStudent(student.id, newStudent);
+            setStudent(updatedStudent);
+        }
+    };
+
+    const handleResetStreak = async (student: Tables<"students">) => {
+        const { last_challenge_timestamp } = student;
+        const difference = dayjs().diff(last_challenge_timestamp, "hour");
+        if (difference >= 24) {
+            const newStudent: TablesUpdate<"students"> = {
+                streaks: 0
+            };
+
+            const updatedStudent = await updateStudent(student.id, newStudent);
+            setStudent(updatedStudent);
+        }
+    };
+
     const handleDashboardAuth = async () => {
         const user = await getUser();
 
@@ -45,7 +85,9 @@ const useAuth = () => {
             return;
         }
 
-        fetchStudent(user);
+        const student = await fetchStudent(user);
+        await handleResetOnLoad(student);
+        await handleResetChallengeAttempt(student);
     };
 
     return { fetchStudent, handleDashboardAuth, handleAuthenticatedUser };
