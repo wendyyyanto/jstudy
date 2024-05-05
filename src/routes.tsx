789859440172d0f8cc9e1517baa7f7ed40aec820
Wendy <wendyanto.bukitvista@gmail.com>
2024-05-05 15:37:57 +0700

    use slug instead of id

--- src/routes.tsx
+++ src/routes.tsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
         path: "courses",
         children: [
             {
-                path: ":courseId/modules/:moduleId",
+                path: ":courseSlug/modules/:moduleId",
                 element: <CourseModule />
             }
         ]
