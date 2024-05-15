bb92cfc439402aebd924c6d67b1b674d29225770
Wendy <wendyanto.bukitvista@gmail.com>
2024-05-15 22:04:14 +0700

    add tables type

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,3 +4,10 @@ export type Tables<T extends keyof Database["public"]["Tables"]> = Database["pub
 export type InsertTables<T extends keyof Database["public"]["Tables"]> = Database["public"]["Tables"][T]["Insert"];
 export type UpdateTables<T extends keyof Database["public"]["Tables"]> = Database["public"]["Tables"][T]["Update"];
 export type Enums<T extends keyof Database["public"]["Enums"]> = Database["public"]["Enums"][T];
+
+export type Course = Tables<"courses"> | null;
+export type Module = Tables<"course_modules"> | null;
+export type StudentCourse = Tables<"student_courses"> | null;
+export type Courses = Course[] | null;
+export type Modules = Module[] | null;
+export type StudentCourses = StudentCourse[] | null;
