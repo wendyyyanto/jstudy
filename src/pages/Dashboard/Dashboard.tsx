28b9e0f937aebefb08c0ca6322d6e34652019e7d
Wendy <wendyanto.bukitvista@gmail.com>
2024-05-19 00:17:40 +0700

    simple framer motion animation

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion, stagger, useAnimate } from "framer-motion";
 
 import useStudentContext from "@/context/studentContext";
 
@@ -19,6 +20,9 @@ import { useCourses } from "../Courses/hooks/useCourses";
 function DashboardPage() {
     useUpdateStudentSubscription();
 
+    const [scope, animate] = useAnimate();
+    const staggerOptions = stagger(0.1, { startDelay: 0.15 });
+
     const { getStudentAchievements } = useAchievementApi();
     const { getLatestAccessedCourse } = useCoursesApi();
 
@@ -28,6 +32,20 @@ function DashboardPage() {
     const { setLastAccessedCourse, lastAccessedCourse } = useCoursesContext();
 
     const [achievementList, setAchievementList] = useState<Tables<"achievements">[] | null>(null);
+
+    useEffect(() => {
+        console.log(scope);
+        animate(
+            "div#info-stagger",
+            {
+                opacity: 1,
+                scale: 1
+            },
+            { duration: 0.2, delay: staggerOptions }
+        );
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     useEffect(() => {
         if (student) {
@@ -56,27 +74,39 @@ function DashboardPage() {
         <>
             <h1 className="text-h2-semibold text-stroke-600 mb-5">Dashboard</h1>
 
-            <div className="grid grid-cols-10 grid-rows-7 gap-5">
-                <div className="col-span-4 row-span-2 bg-highlight-300 rounded-sm flex flex-col px-8 py-6">
+            <div className="grid grid-cols-10 grid-rows-7 gap-5" ref={scope}>
+                <div
+                    id="info-stagger"
+                    className="col-span-4 row-span-2 bg-highlight-300 rounded-sm flex flex-col px-8 py-6"
+                >
                     <LastModuleInfo course={lastAccessedCourse} />
                 </div>
 
-                <div className="relative col-span-4 row-span-2 bg-stroke-900 rounded-sm flex items-center pl-10 gap-8 overflow-hidden">
+                <div
+                    id="info-stagger"
+                    className="relative col-span-4 row-span-2 bg-stroke-900 rounded-sm flex items-center pl-10 gap-8 overflow-hidden"
+                >
                     <RankInfo rank={student.rank} />
                 </div>
 
-                <div className="col-span-2 col-start-9 row-span-1 bg-secondary rounded-sm flex items-center gap-4 p-4">
+                <div
+                    id="info-stagger"
+                    className="col-span-2 col-start-9 row-span-1 bg-secondary rounded-sm flex items-center gap-4 p-4"
+                >
                     <LevelInfo level={student.level} />
                 </div>
 
-                <div className="col-span-2 col-start-9 row-span-1 bg-tertiary-500 rounded-sm flex items-center gap-4 p-4">
+                <div
+                    id="info-stagger"
+                    className="col-span-2 col-start-9 row-span-1 bg-tertiary-500 rounded-sm flex items-center gap-4 p-4"
+                >
                     <PointInfo points={student.points} />
                 </div>
 
-                <WhiteCard className="col-span-6 col-start-1 row-span-5">
+                <WhiteCard id="info-stagger" className="col-span-6 col-start-1 row-span-5">
                     <LearningProgress courses={studentCourses} />
                 </WhiteCard>
-                <WhiteCard className="col-span-4 row-span-5">
+                <WhiteCard id="info-stagger" className="col-span-4 row-span-5">
                     {achievementList && achievementList.length > 0 ? (
                         <Achievements achievements={achievementList} />
                     ) : (
